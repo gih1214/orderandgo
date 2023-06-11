f66b949db3ef598a88330eea8a9320f9e04cd161
zyzyzyclaire <kunji117@naver.com>
2023-06-11 11:43:16 +0900

    models-1

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,7 @@
 from app import db
+from datetime import datetime
 
+'''
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
@@ -28,4 +30,135 @@ class Test(db.Model):
 
     def __repr__(self):
         return f'<Post {self.title}>'
-    
+'''
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(50), unique=True)
+    birthday = db.Column(db.Date, nullable=False)
+    tel = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True)
+    address = db.Column(db.String(150), nullable=False)
+    created_at = db.Column(db.datetime, default=datetime.now())
+    last_logged_at = db.Column(db.datetime, nullable=True)
+
+
+class Store(db.Model):
+    __tablename__ = 'store'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, foreign_key=('user.id'))
+    name = db.Column(db.String(50), unique=True)
+    address = db.Column(db.String(150), nullable=False)
+    tel = db.Column(db.String(50), nullable=False)
+    manager_name = db.Column(db.String(50), nullable=False)
+    manager_tel = db.Column(db.String(50), nullable=False)
+    logo_img = db.Column(db.String(150), nullable=False)
+    store_image = db.Column(db.String(150), nullable=False)
+    main_description = db.Column(db.Text, nullable=False)
+    sub_description = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.datetime, default=datetime.now())
+    last_logged_at = db.Column(db.datetime, nullable=True)
+
+
+class TableCategory(db.Model):
+    __tablename__ = 'table_category'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    store_id = db.Column(db.Integer, foreign_key=('store.id'))
+    category_name = db.Column(db.String(50), nullable=True)
+
+
+class Table(db.Model):
+    __tablename__ = 'table'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150), nullable=True)
+    number = db.Column(db.Integer, nullable=True)
+    seat_count = db.Column(db.Integer, nullable=True)
+    table_category_id = db.Column(db.Integer, foreign_key=('table_category.id'))
+
+
+class MainCategory(db.Model):
+    __tablename__ = 'main_category'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    store_id = db.Column(db.Integer, foreign_key=('store.id'))
+    name = db.Column(db.String(150), nullable=False)
+
+
+class SubCategory(db.Model):
+    __tablename__ = 'sub_category'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    main_category_id = db.Column(db.Integer, foreign_key=('main_category.id'))
+    name = db.Column(db.String(150), nullable=False)
+
+
+class MenuHasCategory(db.Model):
+    __tablename__ = 'menu_has_category'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    main_category_id = db.Column(db.Integer, foreign_key=('main_category.id'))
+    sub_category_id = db.Column(db.Integer, foreign_key=('sub_category.id'), nullable=True)
+
+
+class MenuOption(db.Model):
+    __tablename__ = 'menu_option'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    store_id = db.Column(db.Integer, foreign_key=('store.id'))
+
+
+class Menu(db.Model):
+    __tablename__ = 'menu'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.String(150), nullable=False)
+    main_description = db.Column(db.Text, nullable=True)
+    sub_description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.datetime, default=datetime.now())
+    store_id = db.Column(db.Integer, foreign_key=('store.id'))
+    menu_has_category_id = db.Column(db.Integer, foreign_key=('manu_has_category.id'))
+    menu_option_id = db.Column(db.Integer, foreign_key=('manu_option.id'))
+
+
+class OrderStatus(db.Model):
+    __tablename__ = 'order_status'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    status = db.Column(db.String(150), nullable=False)
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ordered_at = db.Column(db.datetime, default=datetime.now())
+    order_status_id = db.Column(db.Integer, foreign_key=('order_status.id'))
+    menu_id = db.Column(db.Integer, foreign_key=('menu.id'))
+    table_id = db.Column(db.Integer, foreign_key=('table.id'))
+
+
+class TableOrderList(db.Model):
+    __tablename__ = 'table_order_list'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    table_id = db.Column(db.Integer, foreign_key=('table.id'))
+    ordered_id = db.Column(db.Integer, foreign_key=('order.id'))
+    checkingin_at = db.Column(db.Datetime, nullable=False)
+    checkingout_at = db.Column(db.Datetime, nullable=False)
+    order_status_id = db.Column(db.Integer, foreign_key=('order_status.id'))
+
+
+class Payment_method(db.Model):
+    __tablename__ = 'payment_method'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    method = db.Column(db.String(50), nullable=False)
+
+
+class Payment(db.Model):
+    __tablename__ = 'payment'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    payment_amount = db.Column(db.Integer, nullable=False)
+    payment_datetime = db.Column(db.Datetime, defalt=datetime.now())
+    store_id = db.Column(db.Integer, foreign_key=('store.id'))
+    payment_method_id = db.Column(db.Integer, foreign_key=('payment_method.id'))
+    table_order_list_id = db.Column(db.Integer, foreign_key=('table_order_list.id'))
