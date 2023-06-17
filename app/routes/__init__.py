1ebc06be954510cdca6d2f1345578700432e6a85
whrksp126 <whrksp126@naver.com>
2023-06-18 03:34:22 +0900

    set order page

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 main_bp = Blueprint('main', __name__)
 auth_bp = Blueprint('auth', __name__)
-pos_bp = Blueprint('pos', __name__)
+pos_bp = Blueprint('pos', __name__, url_prefix='/pos')
 adm_bp = Blueprint('adm', __name__, url_prefix='/adm')
 
 from app.routes import main, auth, pos, adm
