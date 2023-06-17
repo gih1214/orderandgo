40a427d0929d4594bf7e1ad877aa3f7e343beca1
whrksp126 <whrksp126@naver.com>
2023-06-18 00:29:08 +0900

    save adm api

--- app/static/js/adm.js
+++ app/static/js/adm.js
@@ -1,3 +1,4 @@
+// 랜덤한 입력 값 넣기
 function randomizeInputs(formname) {
   const inputs = document.querySelectorAll(`${formname} input`);
   inputs.forEach(input => {
@@ -13,8 +14,10 @@ function randomizeInputs(formname) {
     textarea.value = randomValue;
   });
 }
-const url = 'http://your-api-endpoint.com/user/create'; // API 엔드포인트 URL
 
+const url = 'http://127.0.0.1:5000'; // API 엔드포인트 URL
+
+// form데이터를 이용해 네임과 같은 키 값을 만들고 리턴 함
 function createObjectFromFormData(form) {
   const formData = {};
   for (const element of form.elements) {
@@ -25,9 +28,11 @@ function createObjectFromFormData(form) {
   return formData;
 }
 
+// submit 이벤트를 감지해서 데이터를 추출함
 function handleFormSubmit(formId, processData) {
   const form = document.getElementById(formId);
   form.addEventListener('submit', function(event) {
+    
     event.preventDefault();
     const formData = createObjectFromFormData(form);
     processData(formData);
@@ -36,44 +41,102 @@ function handleFormSubmit(formId, processData) {
 
 handleFormSubmit('userForm', function(userData) {
   console.log(userData);
+  async function main() {
+    try {
+      const response = await submitFun( url+'/adm/user', 'POST', userData);
+      console.log('Received data:', response);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  main();
 });
+
 
 handleFormSubmit('storeForm', function(storeData) {
   console.log(storeData);
+  async function main() {
+    try {
+      const response = await submitFun( url+'/adm/store', 'POST', storeData);
+      console.log('Received data:', response);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  main();
 });
 
 handleFormSubmit('menuForm', function(menuData) {
   console.log(menuData);
+  async function main() {
+    try {
+      const response = await submitFun( url+'/adm/menu', 'POST', menuData);
+      console.log('Received data:', response);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  main();
 });
 
 handleFormSubmit('menuOptionForm', function(menuOptionData) {
   console.log(menuOptionData);
+  async function main() {
+    try {
+      const response = await submitFun( url+'/adm/menu_option', 'POST', menuOptionData);
+      console.log('Received data:', response);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  main();
 });
 
 handleFormSubmit('mainCategoryForm', function(mainCategoryData) {
   console.log(mainCategoryData);
+  async function main() {
+    try {
+      const response = await submitFun( url+'/adm/menu_main_category', 'POST', mainCategoryData);
+      console.log('Received data:', response);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  main();
 });
 
 handleFormSubmit('subCategoryForm', function(subCategoryData) {
   console.log(subCategoryData);
+  async function main() {
+    try {
+      const response = await submitFun( url+'/adm/menu_sub_category', 'POST', subCategoryData);
+      console.log('Received data:', response);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  main();
 });
 
 
 
-// fetch(url, {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify(userData)
-// })
-// .then(response => {
-//   if (response.ok) {
-//     console.log('User created successfully');
-//   } else {
-//     console.log('Failed to create user');
-//   }
-// })
-// .catch(error => {
-//   console.error('Error:', error);
-// });
+async function submitFun(url, method, data) {
+  try {
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+
+    if (response.ok) {
+      return response.json(); // JSON 형태로 응답 데이터를 파싱하여 반환
+    } else {
+      throw new Error('Failed to create user');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
