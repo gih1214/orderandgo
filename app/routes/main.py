f4bccaf00cdd4aa280731ca79410bd7c6c1da7c0
whrksp126 <whrksp126@naver.com>
2023-06-22 22:53:09 +0900

    edit add

--- app/routes/main.py
+++ app/routes/main.py
@@ -3,7 +3,10 @@ from app.routes import main_bp
 
 @main_bp.route('/')
 def index():
-    from app.models.user import create_user
+    from app.models.user import create_user, get_user_by_id
     # create_user('test', 'asd123', '이름', '2000-01-01', '010-0000-0000', 'test@test.co.kr', '부산 어딘가')
+    user = get_user_by_id(15)
+    created_at = user.created_at
+    print(created_at)
     print('실행됨')
     return render_template('index.html')
