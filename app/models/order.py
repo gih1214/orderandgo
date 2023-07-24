daba08fe85e59982dcc481c154cbc016b5022887
zyzyzyclaire <kunji117@naver.com>
2023-07-25 01:20:54 +0900

    주문하기, 결제하기

--- app/models/order.py
+++ app/models/order.py
@@ -1,9 +1,9 @@
 from flask import session, jsonify
-from app.models import db, Table, TableCategory, TableCategoryPage, Order, TableOrderList, Menu
+from app.models import db, Table, TableCategory, TableCategoryPage, Order, TableOrderList, Menu, OrderHasOption
 
 
 # 주문하기 클릭 시
-def make_order(table_id, menu_list):
+def make_order(table_id, order_list):
     table_order_list_item = db.session.query(TableOrderList)\
                                     .filter(TableOrderList.table_id == table_id)\
                                     .filter(TableOrderList.checkingout_at.is_(None))\
@@ -15,15 +15,29 @@ def make_order(table_id, menu_list):
         db.session.add(table_order_list_item)
         db.session.commit()
 
+    for o in order_list:
+        # order_status_id는 임의로 1로 함. temp
+        order_item = Order(order_status_id=1, menu_id=o['menu_id'], table_id=table_id, order_list_id=table_order_list_item.id)
+        db.session.add(order_item)
+        db.session.commit()
+
+        # 옵션이 있으면 옵션 추가
+        if len(o['option_list']) > 0:
+            for oo in o['option_list']:
+                option_order_item = OrderHasOption(order_id=order_item.id, menu_option_id=oo)
+                db.session.add(option_order_item)
+                db.session.commit()
+
+    return True
     
 
 # 결제할 리스트 출력
 def payment_list(table_id):
-    item = db.session.query(Order)\
+    items = db.session.query(Order)\
                     .join(TableOrderList, TableOrderList.id == Order.order_list_id)\
                     .join(Table, Table.id == TableOrderList.table_id)\
                     .filter(TableOrderList.checkingout_at.is_(None), Table.id == table_id)\
                     .all()
-    if item is None:
+    if items is None:
         return "잘못됨"
-    return item
+    return items
