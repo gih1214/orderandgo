880ecd68b5f4aa415c69a2b6b1eb397ca72b65ef
whrksp126 <whrksp126@naver.com>
2023-08-30 03:19:08 +0900

    set menuproduct img

--- app/static/js/setMenuProduct.js
+++ app/static/js/setMenuProduct.js
@@ -1,6 +1,7 @@
 let mainCategoryData;
 let subCategoryData;
 let allMenuData;
+let menuImgData = [];
 
 // 메인카테고리 데이터 가져오기
 fetch(`/store/get_main_category`, {
@@ -43,11 +44,36 @@ fetch(`/store/all_menu_list`, {
   // 받은 데이터 처리
   console.log(data);
   allMenuData = data;
-  // createHtml(data);
+  createMenuTable(data);
 })
 .catch(error => {
   console.error('Error:', error);
 });
+
+// 메뉴 리스트 테이블 html 만들기
+const createMenuTable = (data) => {
+  const html = `
+    <li>
+      <div><input type="checkbox"></div>
+      <div>메인카테고리</div>
+      <div>서브카테고리</div>
+      <div>메뉴명</div>
+      <div>옵션</div>
+      <div>가격</div>
+    </li>
+    ${data.map(({ id, main_category, sub_category, name, price, option })=>`
+    <li data-id="${id}}">
+      <div><input type="checkbox"></div>
+      <div>${main_category}</div>
+      <div>${sub_category}</div>
+      <div>${name}</div>
+      <div>${option}</div>
+      <div>${price.toLocaleString()}</div>
+    </li>`).join('')}
+  `
+  const _menuTable = document.querySelector('.set_menu_product main article .article_bottom ul');
+  _menuTable.innerHTML = html;
+}
 
 // 셀렉트 박스 html 만들기
 const createSeleteBox = (data, fun, target) => {
@@ -92,4 +118,85 @@ const clickCategory = (event) => {
 const clickResponsiveBtn = (event) => {
   const _main = document.querySelector('main');
   _main.classList.toggle('open_aside')
+}
+
+// 이미지 프리뷰 만들기
+function previewImage(event) {
+  const _input = event.target;
+  const _imgBox = event.target.closest('.img_box');
+  const _img = _imgBox.querySelector('img');
+  const _mainImgBox = document.querySelector('.main_img');
+  const _mainImg = _mainImgBox.querySelector('img');
+  const imgBoxIndex = Number(_imgBox.dataset.index);
+  if (_input.files && _input.files[0]) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      _img.setAttribute('src', e.target.result);
+      menuImgData[imgBoxIndex-1] = e.target.result;
+      _mainImg.setAttribute('src', e.target.result);
+    }
+    reader.readAsDataURL(_input.files[0]);
+    _imgBox.classList.add('active');
+    _mainImgBox.classList.add('active');
+    _mainImgBox.dataset.index = imgBoxIndex;
+  }
+}
+// 멀티 이미지 프리뷰 만들기 
+function multiPreviewImage(event) {
+  event.preventDefault();
+  if(_mainInput.files.length > 4) {
+    alert('메뉴 이미지는 최대 4개까지 설정이 가능합니다.')
+    return;
+  }
+  if(!_mainInput.files) return;
+  for (let i = 0; i < _mainInput.files.length && i < 4; i++) {
+    const _imgBox = document.querySelector(`.img_box[data-index="${i+1}"]`);
+    const _img = _imgBox.querySelector('img');
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      _img.setAttribute('src', e.target.result);
+      menuImgData[i] = e.target.result;
+      if(i == 0) {
+        _mainImg.setAttribute('src',e.target.result)
+      }
+      _imgBox.classList.add('active');    
+    }
+    reader.readAsDataURL(_mainInput.files[i]);
+  }
+  _mainImgBox.classList.add('active');
+  _mainImgBox.dataset.index = 1;
+  
+}
+
+// 이미지 클릭 시
+const clickMenuImg = (event) => {
+  const _img = event.currentTarget;
+  const _imgBox = event.target.closest('.img_box');
+  const _mainImgBox = document.querySelector('.main_img');
+  const _mainImg = _mainImgBox.querySelector('img');
+  const imgBoxIndex = Number(_imgBox.dataset.index);
+  _mainImg.setAttribute('src', _img.src);
+  _mainImgBox.classList.add('active');
+  _mainImgBox.dataset.index = imgBoxIndex;
+}
+
+// 이미지 삭제 클릭 시
+const clickDeleteImg = (event) => {
+  const _mainImgBox = event.target.closest('.main_img');
+  const _mainImg = _mainImgBox.querySelector('img');
+  const imgBoxIndex = Number(_mainImgBox.dataset.index);
+  const _imgBox = document.querySelector(`.img_box[data-index="${imgBoxIndex}"]`);
+  const _img = _imgBox.querySelector('img');
+  menuImgData[imgBoxIndex-1] = '';
+  _img.setAttribute('src', '');
+  _mainImg.setAttribute('src', '');
+  _imgBox.classList.remove('active');
+  _mainImgBox.classList.remove('active');
+  _mainImgBox.dataset.index = '';
+  const _activeImg = document.querySelectorAll('.img_box.active');
+  if(_activeImg.length != 0) {
+    _mainImg.setAttribute('src', _activeImg[0].querySelector('img').src); 
+    _mainImgBox.dataset.index = _activeImg[0].dataset.index;
+    _mainImgBox.classList.add('active');
+  }
 }
