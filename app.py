b67eeccefffdc0ee0b5a731a79aa9ed51b4b6817
whrksp126 <whrksp126@gmail.com>
2024-02-14 15:43:21 +0900

    cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from app import create_app, db
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = create_app()
 migrate = Migrate(app, db)
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from contextlib import contextmanager
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, text
-from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()     
@@ -17,7 +16,6 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    CORS(app)
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
